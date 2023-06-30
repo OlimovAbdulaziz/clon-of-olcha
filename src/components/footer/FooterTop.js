87d106329931b07e50a341cf687945f43d29dc1f
OlimovAbdulaziz <abdulazizcoder642@gmail.com>
2023-06-30 04:40:22 +0300

    clon of olcha

--- src/components/footer/FooterTop.js
+++ src/components/footer/FooterTop.js
@@ -7,7 +7,7 @@ function FooterTop() {
             <div className="footer__top-wrapper">
                 <div className="footer__top-details">
                     <h2>Bizning mobil ilovamiz APPGallery, App <br /> Store vs Google Play-da</h2>
-                    <div className="icons">
+                    <div className="footer__icons">
                         <img style={{ paddingLeft: "20px" }} src="https://olcha.uz/_nuxt/appstore.32a5f67a.svg" alt="" />
                         <img src="https://olcha.uz/_nuxt/googleplay.4ba35be8.svg" alt="" />
                         <img src="https://olcha.uz/_nuxt/app-gallery.879faacf.svg" alt="" />
